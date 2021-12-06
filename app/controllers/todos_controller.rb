960a28950476857208f4cd695641087abcc47fb8
harmolipi <nikob381@gmail.com>
2021-12-05 19:49:28 -0500

    Created TodosController methods

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,2 +1,42 @@
 class TodosController < ApplicationController
+  before_action :set_todo, only: [:show, :update, :destroy]
+
+  # GET /todos
+  def index
+    @todos = Todo.all
+    render json: @todos, status: :ok
+  end
+
+  # POST /todos
+  def create
+    @todo = Todo.create!(todo_params)
+    render json: @todo, status: :created
+  end
+
+  # GET /todos/:id
+  def show
+    render json: @todo
+  end
+
+  # PUT /todos/:id
+  def update
+    @todo.update(todo_params)
+    head :no_content
+  end
+
+  # DELETE /todos/:id
+  def destroy
+    @todo.destroy
+    head :no_content
+  end
+
+  private
+
+  def todo_params
+    params.permit(:title, :created_by)
+  end
+
+  def set_todo
+    @todo = Todo.find(params[:id])
+  end
 end
