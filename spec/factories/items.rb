30ef4d800836950858d0263d4e26010843504ff3
harmolipi <nikob381@gmail.com>
2021-12-05 19:19:12 -0500

    Fixed syntax

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,6 +1,7 @@
 FactoryBot.define do
   factory :item do
-    name { Faker::StarWars.character }
-    done falsetodo_id nil
+    name { Faker::Movies::StarWars.character }
+    done { false }
+    todo_id { nil }
   end
 end
